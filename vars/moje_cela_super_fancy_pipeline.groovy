4f9f96da3a3b63282da31ff005e992b2d38d0bec
Mira Hedl <miroslav.hedl@merck.com>
2019-09-24 20:38:51 +0200

    WIP Tue Sep 24 20:38:51 CEST 2019

--- vars/moje_cela_super_fancy_pipeline.groovy
+++ vars/moje_cela_super_fancy_pipeline.groovy
@@ -29,7 +29,7 @@ def call(Map params)
 
             stage('🏗️ Deploys') {
                 parallel {
-                    stage('💻 On Premise') {
+                    stage('On Premise') {
                         stages {
                             stage('📦 Build package') {
                                 steps { sh 'sleep 3'; echo hlaska }
@@ -40,7 +40,7 @@ def call(Map params)
                         }
                     }
 
-                    stage('☁️ AWS') {
+                    stage('AWS') {
                         stages {
                             stage('📦 Build AMI') {
                                 steps { sh 'sleep 1'; echo hlaska }
